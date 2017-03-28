c5e68b71e53ec84d67df7ce3b26fceaa8fafe712
木水 <avatar_007@qq.com>
2017-03-28 15:30:55 +0800

    第一次修改

--- 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
+++ 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
@@ -1,14 +1,22 @@
 package com.studio.googleplay;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("谷歌电子市场");
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(Color.RED);
+        tv.setTextSize(30);
+        setContentView(tv);
 
     }
 }
