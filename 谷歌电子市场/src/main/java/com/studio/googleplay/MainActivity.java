2336561bafa48d65e7666731d277ed8b04ff7c63
木水 <avatar_007@qq.com>
2017-03-28 16:32:02 +0800

    第三次修改

--- 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
+++ 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String name = "oneName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
