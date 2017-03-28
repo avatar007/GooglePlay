ff275f1a8ee135f02122b7797098a16a713a7ddf
木水 <avatar007@users.noreply.github.com>
2017-03-28 16:39:32 +0800

    Update MainActivity.java

--- 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
+++ 谷歌电子市场/src/main/java/com/studio/googleplay/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private String name = "oneName";
+    private String name = "oneNames";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
