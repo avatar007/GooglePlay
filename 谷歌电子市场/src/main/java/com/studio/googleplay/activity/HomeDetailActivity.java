307ea9a1156c04a137173bf3a677ef3d810596ee
木水 <avatar_007@qq.com>
2017-04-07 22:21:01 +0800

    第五天提交

--- 谷歌电子市场/src/main/java/com/studio/googleplay/activity/HomeDetailActivity.java
+++ 谷歌电子市场/src/main/java/com/studio/googleplay/activity/HomeDetailActivity.java
@@ -2,6 +2,8 @@ package com.studio.googleplay.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
@@ -21,67 +23,89 @@ import com.studio.googleplay.view.LoadingPager;
  */
 public class HomeDetailActivity extends BaseActivity {
 
-    private String mPackageName;
-    private AppInfo data;
+	private String mPackageName;
+	private AppInfo data;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        LoadingPager loadingPager = new LoadingPager(this) {
-            @Override
-            public View onCreateSuccessView() {
-                return HomeDetailActivity.this.onCreateSuccessView();
-            }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LoadingPager loadingPager = new LoadingPager(this) {
+			@Override
+			public View onCreateSuccessView() {
+				return HomeDetailActivity.this.onCreateSuccessView();
+			}
 
-            @Override
-            public ResultState onLoad() {
-                return HomeDetailActivity.this.onLoad();
-            }
-        };
-        setContentView(loadingPager);
-        //获取传递过来的包名
-        Intent intent = getIntent();
-        mPackageName = intent.getStringExtra("packageName");
+			@Override
+			public ResultState onLoad() {
+				return HomeDetailActivity.this.onLoad();
+			}
+		};
+		setContentView(loadingPager);
+		// 获取传递过来的包名
+		Intent intent = getIntent();
+		mPackageName = intent.getStringExtra("packageName");
 
-        loadingPager.initData();//初始化数据会走onLoad方法
-    }
+		loadingPager.initData();// 初始化数据会走onLoad方法
+		initActionBar();
+	}
 
-    private View onCreateSuccessView() {
-        View view = UiUtils.inflate(R.layout.page_home_detail);
+	private void initActionBar() {
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setDisplayShowHomeEnabled(true);
+	}
 
-        //添加应用详情页的布局
-        FrameLayout flDetailInfo = (FrameLayout) view.findViewById(R.id.fl_detail_appinfo);
-        DetailAppInfoHolder appInfoHolder = new DetailAppInfoHolder();
-        appInfoHolder.setData(data);
-        flDetailInfo.addView(appInfoHolder.getRootView());
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        //添加安全信息页面的布局
-        FrameLayout flSafeInfo = (FrameLayout) view.findViewById(R.id.fl_detail_safe);
-        DetailSafeHolder safeHolder = new DetailSafeHolder();
-        safeHolder.setData(data);
-        flSafeInfo.addView(safeHolder.getRootView());
+	private View onCreateSuccessView() {
+		View view = UiUtils.inflate(R.layout.page_home_detail);
 
-        //添加截图模块页面布局
-        HorizontalScrollView hsvScreenInfo = (HorizontalScrollView) view.findViewById(R.id.hsv_detail_screen);
-        DetailScreenHolder screenHolder = new DetailScreenHolder();
-        screenHolder.setData(data);
-        hsvScreenInfo.addView(screenHolder.getRootView());
+		// 添加应用详情页的布局
+		FrameLayout flDetailInfo = (FrameLayout) view
+				.findViewById(R.id.fl_detail_appinfo);
+		DetailAppInfoHolder appInfoHolder = new DetailAppInfoHolder();
+		appInfoHolder.setData(data);
+		flDetailInfo.addView(appInfoHolder.getRootView());
 
-        //添加描述模块页面布局
-        FrameLayout flDesInfo = (FrameLayout) view.findViewById(R.id.fl_detail_des);
-        DetailDesHolder desHolder = new DetailDesHolder();
-        desHolder.setData(data);
-        flDesInfo.addView(desHolder.getRootView());
-        return view;
-    }
+		// 添加安全信息页面的布局
+		FrameLayout flSafeInfo = (FrameLayout) view
+				.findViewById(R.id.fl_detail_safe);
+		DetailSafeHolder safeHolder = new DetailSafeHolder();
+		safeHolder.setData(data);
+		flSafeInfo.addView(safeHolder.getRootView());
 
-    private LoadingPager.ResultState onLoad() {
-        HomeDetailProtocol protocol = new HomeDetailProtocol(mPackageName);
-        data = protocol.getData(0);
-        if (data != null) {
-            return LoadingPager.ResultState.STATE_SUCCESS;
-        } else {
-            return LoadingPager.ResultState.STATE_ERROR;
-        }
-    }
+		// 添加截图模块页面布局
+		HorizontalScrollView hsvScreenInfo = (HorizontalScrollView) view
+				.findViewById(R.id.hsv_detail_screen);
+		DetailScreenHolder screenHolder = new DetailScreenHolder();
+		screenHolder.setData(data);
+		hsvScreenInfo.addView(screenHolder.getRootView());
+
+		// 添加描述模块页面布局
+		FrameLayout flDesInfo = (FrameLayout) view
+				.findViewById(R.id.fl_detail_des);
+		DetailDesHolder desHolder = new DetailDesHolder();
+		desHolder.setData(data);
+		flDesInfo.addView(desHolder.getRootView());
+		return view;
+	}
+
+	private LoadingPager.ResultState onLoad() {
+		HomeDetailProtocol protocol = new HomeDetailProtocol(mPackageName);
+		data = protocol.getData(0);
+		if (data != null) {
+			return LoadingPager.ResultState.STATE_SUCCESS;
+		} else {
+			return LoadingPager.ResultState.STATE_ERROR;
+		}
+	}
+
 }
